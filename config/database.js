8bde17a2bd51db3ecef1e7ad62cdc4af04aa034f
webloom123 <shareenpan1@gmail.com>
2026-04-09 11:08:03 +0530

    Fix StackCP exact database host and name

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'SPYTEE',
+  process.env.DB_NAME || 'SPYTEE-35337b6e',
   process.env.DB_USER || 'SPYTEE',
   process.env.DB_PASS || 'Lk8121@Lk8121',
   {
-    host: (!process.env.DB_HOST || process.env.DB_HOST === 'localhost') ? 'spyteetech.site' : process.env.DB_HOST,
+    host: (!process.env.DB_HOST || process.env.DB_HOST === 'localhost' || process.env.DB_HOST === 'spyteetech.site') ? 'shareddb-b.hosting.stackcp.net' : process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     logging: false,
